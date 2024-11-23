21455a8d6108bc02531b3b8e813cabec7e27cc11
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2024-11-23 07:35:56 +0100

    First Commit

--- account_app/views.py
+++ account_app/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import Client,Bank, Account
 from .serializers import ClientSerializer,BankSerializer,AccountSerializer
+
 class ClientViewSet(viewsets.ModelViewSet):
     queryset=Client.objects.all()
     serializer_class=ClientSerializer
@@ -14,3 +18,13 @@ class BankViewSet(viewsets.ModelViewSet):
 class AccountViewSet(viewsets.ModelViewSet):
     queryset=Account.objects.all()
     serializer_class=AccountSerializer
+    """@action(detail=False,methods=['GET'],url_path='overdrawns')
+    def get_overdrawn_accounts(self,request):
+        try:
+            result=Account.objects.filter(balance__lt=0)
+            serializer=AccountSerializer(result,many=True)
+            accounts=serializer.data
+            return HttpResponse(accounts,status.HTTP_200_OK)
+        except Account.DoesNotExist:
+            return HttpResponse('There is no overdrawn accounts.',status=status.HTTP_204_NO_CONTENT)
+    """
